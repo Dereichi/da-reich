832cc7529bbce192d691338b030fafe5be0472b9
Dereichi <dereiki09@gmail.com>
2025-09-29 11:21:16 -0700

    Update contact.js

--- scripts/contact.js
+++ scripts/contact.js
@@ -14,7 +14,7 @@ const feedback = document.getElementById("feedback");
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
-
+ 
   const name = document.getElementById("name").value.trim();
   const email = document.getElementById("email").value.trim();
   const age = document.getElementById("age").value.trim();
