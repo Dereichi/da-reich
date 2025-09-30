00c7c14d05988f60c282ab689068f2473d5697b7
Dereichi <dereiki09@gmail.com>
2025-09-30 07:48:31 -0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 const contactRoutes = require('./routes/contact');
 require('dotenv').config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 app.use('/api/contact', contactRoutes);
 
